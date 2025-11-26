135075ac064e20b86c2b4996ee5fcaf99136e593
raphael <infra@secretariaplus.com.br>
2025-11-26 15:14:05 -0300

    done

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                lp1: resolve(__dirname, 'lp1.html')
+                main: resolve(__dirname, 'index.html')
             }
         }
     }
